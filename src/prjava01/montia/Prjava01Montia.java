ee81fad4e95c1a8568b9606a7d476c4bea2cb6ca
ubu64 <ubu64@ubu64DEV-Montia>
2014-05-14 21:15:17 +0200

    versio 0.1 del projecte prjava01-Montia

---
+++ src/prjava01/montia/Prjava01Montia.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package prjava01.montia;
+
+/**
+ *
+ * @author ubu64
+ */
+
+import java.awt.Desktop;
+import java.io.*;
+
+public class Prjava01Montia {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException {
+
+        File f = new File("montia.html");
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(f))){
+        bw.write("<html>");bw.newLine();
+        bw.write("<head>");bw.newLine();
+        bw.write("<title>");bw.newLine();
+        bw.write("Nova pàgina web");bw.newLine();
+        bw.write("</title>");bw.newLine();
+        bw.write("</head>");bw.newLine();
+        bw.write("<body>");bw.newLine();
+        bw.write("Nova pàgina web de Miquel Montia");
+        bw.newLine();
+        bw.write("</body>");bw.newLine();
+        bw.write("</html>");bw.newLine();
+        bw.close();
+        }
+    
+    }
+    
+}
